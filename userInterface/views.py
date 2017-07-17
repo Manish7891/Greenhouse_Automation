00efab69433fb0b38419f8aa61e60bc53e7128e6
77neel <neelpatel7.np@gmail.com>
2017-07-17 15:54:52 +0530

    i just dele..

--- userInterface/views.py
+++ userInterface/views.py
@@ -4,8 +4,6 @@ from django.shortcuts import render
 def home(request):
     return render(request,'index.html')
 
-def login(request):
-    return render(request,'index.html')
 
 def table(request):
     return render(request,'index.html')
